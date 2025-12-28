62fb53d19eb1aca0d3fd93c6f84f253c58dcbddd
Francisco de Assis Silva <fcodiassis@yahoo.com.br>
2025-12-28 23:59:58 +0000

    Construcao da fato vendas

---
+++ models/staging/sap_nwind/stg_sap_nwind__order_details.sql
@@ -0,0 +1,23 @@
+with 
+
+source_orders_details as (
+
+    select * from {{ source('sap_nwind', 'order_details') }}
+
+),
+
+renamed as (
+
+    select
+        {{ dbt_utils.generate_surrogate_key(['order_id','product_id'])}} as order_item_pk
+        ,cast(order_id as int) as order_fk
+        ,cast(product_id as int) as product_fk
+        ,cast(unit_price as float) as unit_price
+        ,cast(quantity as int) as quantity
+        ,cast(discount as float) as discount_pct
+
+    from source_orders_details
+
+)
+
+select * from renamed
