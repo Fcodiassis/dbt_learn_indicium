4cee58da6e5b4d1edfe9708f87ee46abde22fbe1
Francisco de Assis Silva <fcodiassis@yahoo.com.br>
2026-01-15 21:02:02 +0000

    inclusao do modelo intermediario de pedidos de vendas relacionado com os itens de vendas

---
+++ models/staging/sap_aworks/stg_sap_aworks__sales_salesorderheader.sql
@@ -0,0 +1,42 @@
+with 
+
+source as (
+
+    select * from {{ source('sap_aworks', 'sales_salesorderheader') }}
+
+),
+
+renamed as (
+
+    select
+        salesorderid as pedido_id,
+        --revisionnumber,
+        orderdate as data_pedido,
+        --duedate,
+        shipdate as data_envio,
+        status,
+        --onlineorderflag,
+        --purchaseordernumber,
+        --accountnumber,
+        customerid,
+        --salespersonid,
+        --territoryid,
+        billtoaddressid as id_ender_cobranca,
+        shiptoaddressid as id_ender_envio,
+        shipmethodid,
+        creditcardid,
+        --creditcardapprovalcode,
+        currencyrateid,
+        subtotal as vlr_subtotal,
+        taxamt as vlt_tota_imposto,
+        freight as vlr_total_frete,
+        totaldue as vlr_total,
+        --comment,
+        --rowguid,
+        modifieddate
+
+    from source
+
+)
+
+select * from renamed
